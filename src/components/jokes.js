20f896e81f2959d1c69778f5a94f45be10ce6197
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2023-06-13 17:20:49 -0600

    set up

---
+++ src/components/jokes.js
@@ -0,0 +1,53 @@
+import { fetchData } from './fetch.js   ';
+
+// Función para obtener un chiste aleatorio
+export async function getJoke(API_URL,joke) {
+  try {
+    const data = await fetchData(API_URL);
+    // Actualiza el texto del chiste en el elemento <p>
+
+    joke.textContent = data.joke;
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Función para buscar chistes que coincidan con un término
+export async function searchJoke() {
+  const searchTerm = document.getElementById('searchInput').value;
+  const search_URL = `https://icanhazdadjoke.com/search?term=${searchTerm}`;
+
+  try {
+
+    const data = await fetchData(search_URL);
+    const jokeList = document.getElementById('jokeList');
+    jokeList.innerHTML = '';
+
+    if (data.results.length > 0 && searchTerm != 0) {
+      const jokesFragment = document.createDocumentFragment(); // Crea un fragmento de documento para agregar los chistes
+    
+      for (const result of data.results) {
+        const jokeItem = document.createElement('li'); // Crea un elemento de lista para cada chiste
+        const jokeLink = document.createElement('a'); // Crea un enlace para cada chiste
+        jokeLink.textContent = result.joke;
+        jokeLink.classList.add("joke--link")
+        jokeLink.href = `eshop.html?joke=${result.joke}`;
+        jokeItem.appendChild(jokeLink); // Agrega el enlace al elemento de lista
+        jokesFragment.appendChild(jokeItem); // Agrega el elemento de lista al fragmento de documento
+      }
+    
+      // Agrega todos los chistes al DOM de una sola vez
+      jokeList.appendChild(jokesFragment);
+    
+      jokeList.style.display = 'block';
+    } else {
+      const noResultsItem = document.createElement('li');
+      noResultsItem.textContent = 'No se encontraron chistes con ese término de búsqueda.';
+      jokeList.appendChild(noResultsItem);
+      jokeList.style.display = 'block';
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { getJoke, searchJoke } from '../src/components/jokes.js';
+
+const API_URL = 'https://icanhazdadjoke.com/'
+
+// Evento de clic para el botón de chiste aleatorio
+const randomJoke = document.getElementById("jokeText");
+document.getElementById('jokeButton').addEventListener('click', async () => {
+  await getJoke(API_URL,randomJoke);
+  randomJoke.href = `eshop.html?joke=${randomJoke.textContent}`;
+  randomJoke.classList.add("joke--link");
+});
+
+// Evento de envío del formulario
+document.getElementById('searchForm').addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevenir el envío del formulario predeterminado
+  searchJoke();
+});
